a0020cfdb040ca7623bbc09c54789c3e889e5911
Aman Chokshi <achokshi@student.unimelb.edu.au>
2021-10-19 20:08:20 +1100

    Cleaned up rts submit script

--- 1120082744_template/sRTS_1120082744.sh
+++ 1120082744_template/sRTS_1120082744.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -l
 #SBATCH --job-name="RTS"
 #SBATCH -o RTS-%A.out
+#SBATCH -e RTS-%A.err
 #SBATCH --nodes=25
 #SBATCH --ntasks-per-node=1
 #SBATCH --time=00:40:00
@@ -12,7 +13,7 @@
 cd $SLURM_SUBMIT_DIR
 
 srun --export=ALL --mem=20000 --ntasks=25  --nodes=25 --gres=gpu:1  --ntasks-per-node=1 \
-    /fred/oz048/bpindor/mwa-RTS/bin/rts_gpu rts_rm_sim.in > srun.${SLURM_JOB_ID}_2.log
+    /fred/oz048/bpindor/mwa-RTS/bin/rts_gpu rts_rm_sim.in > srun.${SLURM_JOB_ID}.log
 
 mkdir -p imgs/instr && mkdir -p imgs/stokes
 mv *img*.fits imgs/instr
