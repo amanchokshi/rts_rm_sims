f827ca5faaeca96c4e32bbf3273e984706aac74a
Aman Chokshi <amanchokshi.iisc@gmail.com>
2021-10-20 10:00:51 +1300

    Cleaned up submit script

--- 1120082744_template/sRTS_1120082744.sh
+++ 1120082744_template/sRTS_1120082744.sh
@@ -4,7 +4,7 @@
 #SBATCH -e RTS-%A.err
 #SBATCH --nodes=25
 #SBATCH --ntasks-per-node=1
-#SBATCH --time=00:40:00
+#SBATCH --time=04:00:00
 #SBATCH --partition=skylake-gpu
 #SBATCH --account=oz048
 #SBATCH --export=NONE
@@ -13,8 +13,8 @@
 cd $SLURM_SUBMIT_DIR
 
 srun --export=ALL --mem=20000 --ntasks=25  --nodes=25 --gres=gpu:1  --ntasks-per-node=1 \
-    /fred/oz048/bpindor/mwa-RTS/bin/rts_gpu rts_rm_sim.in > srun.${SLURM_JOB_ID}.log
+    /fred/oz048/bpindor/mwa-RTS/bin/rts_gpu rts_rm_sim.in > "srun.${SLURM_JOB_ID}.log"
 
 mkdir -p imgs/instr && mkdir -p imgs/stokes
-mv *img*.fits imgs/instr
-mv *stokes*.fits imgs/stokes
+mv ./*img*.fits imgs/instr
+mv ./*stokes*.fits imgs/stokes
