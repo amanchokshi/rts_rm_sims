e803ec6fd320fc590d107367dec8364ab191139e
Aman Chokshi <achokshi@student.unimelb.edu.au>
2021-10-20 14:17:08 +1100

    It finially makes fits cubes on ozstar

--- rts_cube/cube_rts.sh
+++ rts_cube/cube_rts.sh
@@ -12,7 +12,8 @@
 #SBATCH -e cube-%A.err
 
 WORKDIR=$1
-data_dir="/fred/oz048/achokshi/rts_rm_sims/${WORKDIR}"
+RTS_RM=/fred/oz048/achokshi/rts_rm_sims/
+DATA_DIR="${RTS_RM}/${WORKDIR}"
 
 module purge
 module load gcc/6.4.0
@@ -21,5 +22,5 @@ module load python/3.6.4
 module load numpy/1.16.3-python-3.6.4
 module load astropy/3.1.2-python-3.6.4
 
-time python "${data_dir}"/cube_rts/cube_rts.py \
-    --fits_dir="$data_dir"/imgs/stokes --out_dir="$data_dir"/imgs/cubes
+time python "${RTS_RM}"/rts_cube/cube_rts.py \
+    --fits_dir="${DATA_DIR}/imgs/stokes" --out_dir="${DATA_DIR}/imgs/cubes"
