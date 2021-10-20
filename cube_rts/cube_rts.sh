c1e7984f828ed781a9613f2e750b3ecae5d71000
Aman Chokshi <achokshi@student.unimelb.edu.au>
2021-10-20 13:46:15 +1100

    Fixed paths and python now working with new bashrc etc

--- cube_rts/cube_rts.sh
+++ cube_rts/cube_rts.sh
@@ -2,7 +2,7 @@
 
 #SBATCH --nodes=1
 #SBATCH --time=00:30:00
-#SBATCH --partition=skylake-gpu
+#SBATCH --partition=skylake
 #SBATCH --account=oz048
 #SBATCH --nodes=1
 #SBATCH --mem=30gb
@@ -14,9 +14,12 @@
 WORKDIR=$1
 data_dir="/fred/oz048/achokshi/rts_rm_sims/${WORKDIR}"
 
-module load python/3.8.5
-module load numpy
-module load astropy
+module purge
+module load gcc/6.4.0
+module load openmpi/3.0.0
+module load python/3.6.4
+module load numpy/1.16.3-python-3.6.4
+module load astropy/3.1.2-python-3.6.4
 
-time python "${WORKDIR}"/cube_rts/cube_rts.py \
+time python "${data_dir}"/cube_rts/cube_rts.py \
     --fits_dir="$data_dir"/imgs/stokes --out_dir="$data_dir"/imgs/cubes
