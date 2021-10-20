ddf54acfae4379da8b09e984177045bacfdc247c
Aman Chokshi <achokshi@student.unimelb.edu.au>
2021-10-20 23:45:54 +1100

    Installed and got cuffs running but output cubes seem wrong

--- cuffs/cuffs.sh
+++ cuffs/cuffs.sh
@@ -1,14 +1,14 @@
 #!/bin/bash -l
 
 #SBATCH --job-name="cuFFS"
-#SBATCH -o RTS-%A.out
-#SBATCH -e RTS-%A.err
+#SBATCH -o cuffs-%A.out
+#SBATCH -e cuffs-%A.err
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-gpu=1
 #SBATCH --time=00:10:00
 #SBATCH --partition=skylake-gpu
 #SBATCH --account=oz048
+#SBATCH --mem=20000
 #SBATCH --gres=gpu:1
 
 module purge
@@ -21,9 +21,10 @@ module load cfitsio/3.480
 # Other build modules.
 module use /fred/oz048/achokshi/software/modulefiles
 module load libconfig/master
+module load cuFFS/master
 
 # time rmsynthesis /astro/mwaeor/achokshi/rm-synth/slurm/cuffs/cuffs_parset.in
-time rmsynthesis /astro/mwaeor/achokshi/rm-synth/slurm/cuffs/"$1"
+time rmsynthesis /fred/oz048/achokshi/rts_rm_sims/cuffs/"$1"
 
 # remove temp in file
-rm /astro/mwaeor/achokshi/rm-synth/slurm/cuffs/"$1"
+# rm /astro/mwaeor/achokshi/rm-synth/slurm/cuffs/"$1"
