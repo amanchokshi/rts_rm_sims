2eb5759c8da50fa28a9bc993ecb2b8192393899d
Aman Chokshi <amanchokshi.iisc@gmail.com>
2022-02-18 16:05:21 +1300

    Added cuffs to this beautiful pipeline

--- cuffs/cuffs.sh
+++ cuffs/cuffs.sh
@@ -11,20 +11,19 @@
 #SBATCH --mem=20000
 #SBATCH --gres=gpu:1
 
-module purge
-module load cuda/11.1.1
-module load gcc/9.2.0
-module load openmpi/4.0.2
-module load hdf5/1.10.6
-module load cfitsio/3.480
+# module purge
+# module load cuda/11.1.1
+# module load gcc/9.2.0
+# module load openmpi/4.0.2
+# module load hdf5/1.10.6
+# module load cfitsio/3.480
+
+ml purge
+ml gcc/6.4.0 openmpi/3.0.0 gnuplot/5.2.4 cfitsio/3.450 hdf5/1.10.5 cuda/11.1.1
 
 # Other build modules.
 module use /fred/oz048/achokshi/software/modulefiles
 module load libconfig/master
-module load cuFFS/master
+# module load cuFFS/master
 
-# time rmsynthesis /astro/mwaeor/achokshi/rm-synth/slurm/cuffs/cuffs_parset.in
-time rmsynthesis /fred/oz048/achokshi/rts_rm_sims/cuffs/"$1"
-
-# remove temp in file
-# rm /astro/mwaeor/achokshi/rm-synth/slurm/cuffs/"$1"
+time /fred/oz048/achokshi/software/cuFFS/git-repo/rmsynthesis PARSET
--- setup_run_sim.sh
+++ setup_run_sim.sh
@@ -54,13 +54,23 @@ echo -en "Optional - enter suffix for output directory " && read -r SUF
 WORKDIR="1120082744_${SIM}_${CAL}_${SUF}"
 mkdir "$WORKDIR"
 cp -r ./rts_rm_pipeline/* "$WORKDIR"
+cp -r ./cuffs "$WORKDIR"
 cd "$WORKDIR"
+
+# Path to image cubes after rts & cubing the stokes images
+CUBEDIR="${PWD}/imgs/cubes"
+PARSET=${PWD}/cuffs/cuffs_parset.in
+TAG="1120082744_${SIM}_${CAL}_${SUF}_"
 
 echo "Running the RTS from ./${WORKDIR}"
 
-sed -i "s/META/${META}/g" rts_rm_sim.in
-sed -i "s/WORKDIR/${WORKDIR}/g" rts_rm_sim.in
-sed -i "s/CALBEAM/${CALBEAM}/g" rts_rm_sim.in
+sed -i "s|META|${META}|g" rts_rm_sim.in
+sed -i "s|WORKDIR|${WORKDIR}|g" rts_rm_sim.in
+sed -i "s|CALBEAM|${CALBEAM}|g" rts_rm_sim.in
+sed -i "s|PARSET|${PARSET}|g" cuffs/cuffs.sh
+sed -i "s|CUBEDIR|${CUBEDIR}|g" cuffs/cuffs_parset.in
+sed -i "s|TAG|${TAG}|g" cuffs/cuffs_parset.in
 
-SETUP_JOB_0=$(sbatch  ./sRTS_1120082744.sh | cut -d " " -f 4)
-REFLAG_JOB_0=$(sbatch  --dependency=afterok:$SETUP_JOB_0 ./rts_cube/cube_rts.sh $WORKDIR | cut -d " " -f 4)
+RTS_JOB=$(sbatch  ./sRTS_1120082744.sh | cut -d " " -f 4)
+CUBE_JOB=$(sbatch  --dependency=afterok:$RTS_JOB ./rts_cube/cube_rts.sh $WORKDIR | cut -d " " -f 4)
+CUFFS_JOB=$(sbatch  --dependency=afterok:$CUBE_JOB ./cuffs.cuffs.sh | cut -d " " -f 4)
