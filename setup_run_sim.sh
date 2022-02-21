d865d8dbf87ce2702c5b0652d057e6f5cb8753e2
Aman Chokshi <amanchokshi.iisc@gmail.com>
2022-02-21 20:14:41 +1300

    Select rts srclist cfg file

--- setup_run_sim.sh
+++ setup_run_sim.sh
@@ -4,73 +4,110 @@ echo '---------------------------'
 echo "Configure RTS RM SIMULATION"
 echo -e '---------------------------\n'
 
-# Beam model for sim
-while true
-do
- read -r -p "Use deformed FEE model for simulation? [Y/n] " input
+#####################################
+#                                   #
+#   Make RTS cal and sim srclists   #
+#                                   # 
+#####################################
 
- case $input in
-     [yY][eE][sS]|[yY])
- META='1120082744_DipAmps'
- SIM=DEF
- break
- ;;
-     [nN][oO]|[nN])
- META='1120082744'
- SIM=FEE
- break
-        ;;
-     *)
- echo "Invalid input..."
- ;;
- esac
+yml_cfg_path=${PWD}/rts_rm_pipeline/rts/srclists/srclist_cfgs/
+
+cd $yml_cfg_path || exit
+srclist_files=(*.yaml)
+cd - >/dev/null
+
+PS3="Enter File Index, or [0] to exit: "
+echo "Select a RTS sourcelist config file:"
+select file in "${srclist_files[@]}"; do
+    if [[ $REPLY == "0" ]]; then
+        exit
+    elif [[ -z $file ]]; then
+        echo 'Invalid choice, try again' >&2
+    else
+         yml_cfg=${PWD}/${file}
+         echo "RTS srclist config file is: ${yml_cfg}"
+        break
+    fi
 done
 
 
-# Beam model for calibration
-while true
-do
- read -r -p "Use deformed FEE model for calibration? [Y/n] " input
+#####################################
+#                                   #
+#     Beam model for simulation     #
+#                                   # 
+#####################################
 
- case $input in
-     [yY][eE][sS]|[yY])
- CALBEAM='1'
- CAL=DEF
- break
- ;;
-     [nN][oO]|[nN])
- CALBEAM='0'
- CAL=FEE
- break
-        ;;
-     *)
- echo "Invalid input..."
- ;;
- esac
-done
-
-echo -en "Optional - enter suffix for output directory " && read -r SUF
-
-WORKDIR="1120082744_${SIM}_${CAL}_${SUF}"
-mkdir "$WORKDIR"
-cp -r ./rts_rm_pipeline/* "$WORKDIR"
-cp -r ./cuffs "$WORKDIR"
-cd "$WORKDIR"
-
-# Path to image cubes after rts & cubing the stokes images
-CUBEDIR="${PWD}/imgs/cubes"
-PARSET=${PWD}/cuffs/cuffs_parset.in
-TAG="1120082744_${SIM}_${CAL}_${SUF}_"
-
-echo "Running the RTS from ./${WORKDIR}"
-
-sed -i "s|META|${META}|g" rts_rm_sim.in
-sed -i "s|WORKDIR|${WORKDIR}|g" rts_rm_sim.in
-sed -i "s|CALBEAM|${CALBEAM}|g" rts_rm_sim.in
-sed -i "s|PARSET|${PARSET}|g" cuffs/cuffs.sh
-sed -i "s|CUBEDIR|${CUBEDIR}|g" cuffs/cuffs_parset.in
-sed -i "s|TAG|${TAG}|g" cuffs/cuffs_parset.in
-
-RTS_JOB=$(sbatch  ./sRTS_1120082744.sh | cut -d " " -f 4)
-CUBE_JOB=$(sbatch  --dependency=afterok:$RTS_JOB ./rts_cube/cube_rts.sh $WORKDIR | cut -d " " -f 4)
-CUFFS_JOB=$(sbatch  --dependency=afterok:$CUBE_JOB ./cuffs/cuffs.sh | cut -d " " -f 4)
+# while true
+# do
+#  read -r -p "Use deformed FEE model for simulation? [Y/n] " input
+# 
+#  case $input in
+#      [yY][eE][sS]|[yY])
+#  META='1120082744_DipAmps'
+#  SIM=DEF
+#  break
+#  ;;
+#      [nN][oO]|[nN])
+#  META='1120082744'
+#  SIM=FEE
+#  break
+#         ;;
+#      *)
+#  echo "Invalid input..."
+#  ;;
+#  esac
+# done
+# 
+# 
+# #####################################
+# #                                   #
+# #     Beam model for calibration    #
+# #                                   # 
+# #####################################
+# 
+# while true
+# do
+#  read -r -p "Use deformed FEE model for calibration? [Y/n] " input
+# 
+#  case $input in
+#      [yY][eE][sS]|[yY])
+#  CALBEAM='1'
+#  CAL=DEF
+#  break
+#  ;;
+#      [nN][oO]|[nN])
+#  CALBEAM='0'
+#  CAL=FEE
+#  break
+#         ;;
+#      *)
+#  echo "Invalid input..."
+#  ;;
+#  esac
+# done
+# 
+# echo -en "Optional - enter suffix for output directory " && read -r SUF
+# 
+# WORKDIR="1120082744_${SIM}_${CAL}_${SUF}"
+# mkdir "$WORKDIR"
+# cp -r ./rts_rm_pipeline/* "$WORKDIR"
+# cp -r ./cuffs "$WORKDIR"
+# cd "$WORKDIR"
+# 
+# # Path to image cubes after rts & cubing the stokes images
+# CUBEDIR="${PWD}/imgs/cubes"
+# PARSET=${PWD}/cuffs/cuffs_parset.in
+# TAG="1120082744_${SIM}_${CAL}_${SUF}_"
+# 
+# echo "Running the RTS from ./${WORKDIR}"
+# 
+# sed -i "s|META|${META}|g" rts_rm_sim.in
+# sed -i "s|WORKDIR|${WORKDIR}|g" rts_rm_sim.in
+# sed -i "s|CALBEAM|${CALBEAM}|g" rts_rm_sim.in
+# sed -i "s|PARSET|${PARSET}|g" cuffs/cuffs.sh
+# sed -i "s|CUBEDIR|${CUBEDIR}|g" cuffs/cuffs_parset.in
+# sed -i "s|TAG|${TAG}|g" cuffs/cuffs_parset.in
+# 
+# RTS_JOB=$(sbatch  ./sRTS_1120082744.sh | cut -d " " -f 4)
+# CUBE_JOB=$(sbatch  --dependency=afterok:$RTS_JOB ./rts_cube/cube_rts.sh $WORKDIR | cut -d " " -f 4)
+# CUFFS_JOB=$(sbatch  --dependency=afterok:$CUBE_JOB ./cuffs/cuffs.sh | cut -d " " -f 4)
